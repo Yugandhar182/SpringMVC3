5445088346235398e55799e49b8f70023f53d78b
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-30 10:45:31 +0530

    Update HelloController.java

--- src/main/java/com/example/MVC2/Controller/HelloController.java
+++ src/main/java/com/example/MVC2/Controller/HelloController.java
@@ -1,15 +1,22 @@
+
 package com.example.MVC2.Controller;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.runApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
 
 @RestController
-@RequestMapping("/springboot")
-public class HelloController {
-	@GetMapping("/hello")
-    public String message() {
-        return "Hello, World!";
+class HelloController{
+    @GetMapping("/")
+    fun hello() : String {
+        return "Hello"
     }
 }
