7ca99c9ad25e9a3242e0726080bfad62dfd197fb
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-30 10:49:24 +0530

    Update HelloController.java

--- src/main/java/com/example/MVC2/Controller/HelloController.java
+++ src/main/java/com/example/MVC2/Controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-class DemoApplication
+class Mvc2Application
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
